898c07f6168fc931bd87b014d168b5d1c9f89222
yohawing <yohawing@gmail.com>
2015-05-09 16:05:10 +0900

    delete images because reduce filesize

--- web/build.sh
+++ web/build.sh
@@ -3,6 +3,6 @@ php -S 0.0.0.0:8000 -t ./build &
 # coffee
 coffee -o build/js -wmc app/coffee/*coffee &
 # sass
-sass --watch app/sass:build/css &
+node-sass --watch app/sass:build/css &
 
 alias killproj='killall php coffee sass'
